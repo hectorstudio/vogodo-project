6e381a6969aed20cd5b24cad6b471a8a4b1e5bda
hectorstudio <tyler.chong707@hotmail.com>
2020-04-27 10:45:05 +0800

    fix: eslint issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Header from "./containers/Header";
-import Home from "./containers/Home";
-import "./App.scss";
+import React from 'react';
+import Header from './containers/Header';
+import Home from './containers/Home';
+import './App.scss';
 
 function App() {
   return (
