927443a81e8f500c714e14a41c21dc26e82b77da
hectorstudio <dev.topking0811@gmail.com>
2020-05-14 09:51:06 +0800

    feat: built mylisting page

--- src/section/Account/MyListing.jsx
+++ src/section/Account/MyListing.jsx
@@ -4,7 +4,10 @@ import ListingItem from "../../components/ListingItem";
 const MyListing = ({setOpenModal}) => {
   return (
     <Fragment>
-      <div className="pane-title">My Listing - 4 Items</div>
+      <div className="pane-header">
+        <div className="pane-title">My Listing - 4 Items</div>
+        <button className="add-button">+ Add a Listing</button>
+      </div>
       <div className="pane-body listing">
         <ListingItem />
         <ListingItem />
