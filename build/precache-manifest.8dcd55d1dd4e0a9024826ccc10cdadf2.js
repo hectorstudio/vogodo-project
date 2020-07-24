411e2351cbb31fa8342aebb86f6a338d1e5f3048
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2020-07-24 10:18:40 -0400

    fix: production update

---
+++ build/precache-manifest.8dcd55d1dd4e0a9024826ccc10cdadf2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0068149e56726a0fb094d5b75e8242f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3511de14297e9ba2e719",
+    "url": "/static/css/2.b8c93cf1.chunk.css"
+  },
+  {
+    "revision": "1905efde5405cafc1d19",
+    "url": "/static/css/main.ea2d10d4.chunk.css"
+  },
+  {
+    "revision": "3511de14297e9ba2e719",
+    "url": "/static/js/2.e8085edb.chunk.js"
+  },
+  {
+    "revision": "06f06b29fc5a60132840db7989884674",
+    "url": "/static/js/2.e8085edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1905efde5405cafc1d19",
+    "url": "/static/js/main.b6bd16f4.chunk.js"
+  },
+  {
+    "revision": "2abc5c5f2c44452a3fda",
+    "url": "/static/js/runtime-main.79fc4c5d.js"
+  },
+  {
+    "revision": "192749262598d04371b93109cac1f383",
+    "url": "/static/media/avatar.19274926.jpg"
+  },
+  {
+    "revision": "0ba2cd0aaef20c8bc8b90c8616558c7c",
+    "url": "/static/media/banner1.0ba2cd0a.jpg"
+  },
+  {
+    "revision": "c9bbeb6453d9e03cc99d9a25ff2642da",
+    "url": "/static/media/constructed.c9bbeb64.svg"
+  },
+  {
+    "revision": "0ef517abb9c8c4067f7bef954402b03a",
+    "url": "/static/media/feet.0ef517ab.svg"
+  },
+  {
+    "revision": "b9fe9a7d2ee2526933d63caf9aa98835",
+    "url": "/static/media/garage.b9fe9a7d.svg"
+  },
+  {
+    "revision": "a5882892f4d4ce2f3590d15263c8f3b2",
+    "url": "/static/media/introduction.a5882892.png"
+  },
+  {
+    "revision": "f1b886643fd22aee2956e6cfbd3ef118",
+    "url": "/static/media/introduction.f1b88664.mp4"
+  },
+  {
+    "revision": "a0bf434612e980a12ce870d5396cb28f",
+    "url": "/static/media/logo.a0bf4346.png"
+  },
+  {
+    "revision": "bbb1b42a40d1b736a9cff77cb595c92a",
+    "url": "/static/media/lot.bbb1b42a.svg"
+  },
+  {
+    "revision": "e7359e224f86240876e0992fb235b05a",
+    "url": "/static/media/profile.e7359e22.jpg"
+  },
+  {
+    "revision": "c20f8c49aae0771cf135bc837d72c8c6",
+    "url": "/static/media/profilemap.c20f8c49.png"
+  },
+  {
+    "revision": "a153a99924e036ba82e9b331e72b636d",
+    "url": "/static/media/realestate.a153a999.jpg"
+  },
+  {
+    "revision": "1508314a424f698ae68fffa6f2e67357",
+    "url": "/static/media/realestate1.1508314a.jpg"
+  },
+  {
+    "revision": "00aefa9bd71535b42480028aa0bada91",
+    "url": "/static/media/realestate2.00aefa9b.jpg"
+  },
+  {
+    "revision": "c1e5351b58faa349bcf3be7f5c26c195",
+    "url": "/static/media/rent.c1e5351b.svg"
+  },
+  {
+    "revision": "df79340a189ced6afb171f199dcde812",
+    "url": "/static/media/roof.df79340a.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7233870683a3ae90d38b8c79a1f985af.js"
+  "/precache-manifest.8dcd55d1dd4e0a9024826ccc10cdadf2.js"
 );
 
 self.addEventListener('message', (event) => {
